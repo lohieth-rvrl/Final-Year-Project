437d6763587133407f9dbc89c0428aa12132cecf
lohieth-rvrl <lohiethrvrl212@gmail.com>
2025-08-02 16:07:04 +0530

    updated frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Impedit aperiam nulla vero eos! Aperiam itaque, possimus corporis tempora, rem neque dolore natus, voluptatibus recusandae nobis non? Veritatis quisquam cumque sed?</p>
+    </div>
+  )
+}
+
+export default App
