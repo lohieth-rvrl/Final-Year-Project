ada8537fb7972c739bd77ee576ee7ca525e0dfc3
lohieth-rvrl <lohiethrvrl212@gmail.com>
2025-08-02 16:18:10 +0530

    bootstrap included

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,8 +2,10 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div>
-      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Impedit aperiam nulla vero eos! Aperiam itaque, possimus corporis tempora, rem neque dolore natus, voluptatibus recusandae nobis non? Veritatis quisquam cumque sed?</p>
+    <div className='container mt-5'>
+      <h1 className='text-3xl font-bold underline text-center'>
+        Welcome to Edutech
+      </h1>
     </div>
   )
 }
